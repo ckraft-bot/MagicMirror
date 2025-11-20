35c0c9d0d3f127b23e88f86dc8b75ba50ed5a72e
Claire Kraft <c.kraft.9306@gmail.com>
2025-11-19 22:36:39 -0500

    better sheel script

--- mm.sh
+++ mm.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-# sleep for 20s before starting
+# ---------------------------------------------
+# MagicMirror startup script for Raspberry Pi
+# ---------------------------------------------
+
+# Wait a bit before starting (optional)
 sleep 20
 
-# go to MagicMirror directory
+# Navigate to MagicMirror directory
 cd /home/pi/MagicMirror/ || exit
 
-# launch MagicMirror in the background and redirect output to a log file
+# Launch MagicMirror in the background, log output to mm.log
 DISPLAY=:0 npm run start > mm.log 2>&1 &
 
-# save the process ID to a log file so you can stop it later
+# Save the process ID so you can stop it later
 echo $! > mm.pid
 
 echo "MagicMirror started in the background. PID saved in mm.pid"
 
+# ---------------------------------------------
 # To stop MagicMirror later:
-kill $(cat /home/pi/MagicMirror/mm.pid)
-rm /home/pi/MagicMirror/mm.pid
+# kill $(cat /home/pi/MagicMirror/mm.pid)
+# rm /home/pi/MagicMirror/mm.pid
 
 
+# To start/stop MagicMirror using pm2 commands
 # cd ~/MagicMirror
 # to start the mirror in the background
 # pm2 start npm --name "mm" -- start
 # then to stop run this command
 # pm2 stop mm
+# ---------------------------------------------
+
+
 
 
